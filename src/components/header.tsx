9dfbffd432a792aa42b99deea7a7ea3c038ab1f3
Ostap Prokhorov <prokhostap@gmail.com>
2025-10-07 19:46:40 +0200

    Made some changes for more clean code

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import { auth0 } from "@/lib/auth0";
-
 export default async function Header() {
-  const session = await auth0.getSession();
-
-  if (!session) {
-    return null;
-  }
-
   return (
     <header className="flex items-center justify-between px-6 py-4 bg-white text-plum border-b-4 border-plum shadow-sm">
       <Link href="/">
@@ -21,12 +13,12 @@ export default async function Header() {
           priority
         />
       </Link>
-      <Link
+      <a
         href="/auth/logout"
         className="bg-plum text-white text-sm font-medium px-4 py-2 rounded-lg shadow-sm transition duration-200 hover:bg-plum/90 hover:shadow-md"
       >
         Log out
-      </Link>
+      </a>
     </header>
   );
 }
