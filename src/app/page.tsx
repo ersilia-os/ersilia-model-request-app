af0812bf2cd1d474383fee298808876d52deacd9
Arnaud <141914477+arobri67@users.noreply.github.com>
2025-10-07 11:11:42 +0200

    add auth0 sdk and signin (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div className="bg-amber-700 w-14">Ersilia</div>;
+import { auth0 } from "@/lib/auth0";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await auth0.getSession();
+  console.log("🟥", session);
+  if (!session) {
+    redirect("/auth/login");
+  }
+  return (
+    <>
+      <div className="bg-amber-700 w-full">
+        Ersilia - Welcome {session.user.name} go to /auth/logout to logout
+      </div>
+    </>
+  );
 }
