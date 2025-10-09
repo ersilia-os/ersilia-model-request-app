af008b94136bcc45e9ef9e6a8dee96b8693fefcd
Lukas Kouril <kouril.lukas@gmail.com>
2025-10-09 16:41:31 +0200

    fix: Container px added

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -1,3 +1,3 @@
 export default function Container({ children }: { children: React.ReactNode }) {
-  return <main className="mx-auto w-full max-w-5xl">{children}</main>;
+  return <main className="mx-auto px-20 w-full max-w-5xl">{children}</main>;
 }
