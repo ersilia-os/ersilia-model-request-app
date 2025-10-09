96b64bb54473a74e2df845f855e3e094b39dd3d5
Ostap Prokhorov <prokhostap@gmail.com>
2025-10-09 16:59:53 +0200

    feat: add model form in compoponents and the route in app/model-form

---
+++ src/app/model-form/page.tsx
@@ -0,0 +1,20 @@
+import ModelMetadataForm from "@/components/Model Form/modelform";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-white py-10">
+      <Card className="w-full max-w-4xl border-2 border-plum rounded-2xl shadow-xl p-6 md:p-10">
+        <CardHeader className="p-0 mb-6 text-center">
+          <h1 className="text-2xl font-semibold text-gray-800">
+            Model Metadata
+          </h1>
+        </CardHeader>
+
+        <CardContent className="p-0">
+          <ModelMetadataForm />
+        </CardContent>
+      </Card>
+    </main>
+  );
+}
