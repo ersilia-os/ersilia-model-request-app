8dd90c02a7225777e982c69efcf729f60f88fecc
Lukas Kouril <kouril.lukas@gmail.com>
2025-10-13 19:35:16 +0200

    fix: types for PDFInfo and PDFMetaData cleaned

--- src/types/pdf-parse.d.ts
+++ src/types/pdf-parse.d.ts
@@ -1,14 +1,4 @@
 declare module "pdf-parse/lib/pdf-parse.js" {
-  interface PDFInfo {
-    PDFFormatVersion?: string;
-    IsAcroFormPresent?: boolean;
-    IsXFAPresent?: boolean;
-    [key: string]: unknown;
-  }
-
-  interface PDFMetadata {
-    [key: string]: unknown;
-  }
 
   function pdf(data: Buffer): Promise<PDFParseResult>;
 
