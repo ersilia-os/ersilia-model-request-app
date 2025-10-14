44b2b750b51e3a9abe5c0f7a5e11c81c26da7f25
Lukas Kouril <kouril.lukas@gmail.com>
2025-10-14 12:06:20 +0200

    fix: processing page

--- src/app/new-model/processing/page.tsx
+++ src/app/new-model/processing/page.tsx
@@ -1,45 +1,29 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import {
-  Empty,
-  EmptyContent,
-  EmptyDescription,
-  EmptyHeader,
-  EmptyMedia,
-  EmptyTitle,
-} from "@/components/ui/empty";
-import { Spinner } from "@/components/ui/spinner";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { SpinnerEmpty } from "@/components/processing/processing";
 
-export function SpinnerEmpty() {
+export default function ProcessingPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const report = sessionStorage.getItem("generatedReport");
+
+      if (report) {
+        router.push("/new-model/metadata");
+      } else {
+        router.push("/new-model");
+      }
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white w-full my-6">
-      <Empty className="w-full shadow-xl border-2 border-solid border-plum rounded-2xl p-6 md:p-8 lg:p-10">
-        <div className="text-center p-0 mb-4">
-          <EmptyHeader>
-            <div className="flex justify-center mb-6">
-              <EmptyMedia variant="icon">
-                <Spinner />
-              </EmptyMedia>
-            </div>
-
-            <EmptyTitle className="text-2xl md:text-3xl lg:text-4xl font-bold text-plum mb-3 md:mb-4">
-              Processing your request
-            </EmptyTitle>
-
-            <EmptyDescription className="text-center text-gray-400 text-sm md:text-base lg:text-lg mb-2">
-              Please wait while we process your request. <br />
-              Do not refresh the page.
-            </EmptyDescription>
-          </EmptyHeader>
-        </div>
-
-        <EmptyContent className="flex justify-center mt-4">
-          <Button variant="transparent" size="lg">
-            Cancel
-          </Button>
-        </EmptyContent>
-      </Empty>
+    <div className="min-h-screen flex items-center justify-center">
+      <SpinnerEmpty />
     </div>
   );
 }
