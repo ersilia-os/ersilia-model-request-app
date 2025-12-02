9abcd06e469e943d72d2f7e89340161b854ae754
mminarro <mminarro18@gmail.com>
2025-12-02 16:11:35 +0100

    Update repository owner and name in PreviewSubmit

--- src/components/preview/PreviewSubmit.tsx
+++ src/components/preview/PreviewSubmit.tsx
@@ -27,8 +27,8 @@ interface PreviewSubmitProps {
 }
 
 export default function PreviewSubmit({ data }: PreviewSubmitProps) {
-  const owner = "arobri67";
-  const repo = "testerbot";
+  const owner = "ersilia-os";
+  const repo = "ersilia";
 
   const router = useRouter();
   const [confirmStep, setConfirmStep] = useState(false);
